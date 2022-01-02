42c767eb35b23b4846e66cc96cdc43aa78b3a26a
James Abley <james.abley@gmail.com>
2022-01-02 23:32:44 +0000

    Add constructors to classes
    aka user-defined initialisation
    
    ```
    class Cake {
      init() {
        this.adjective = "delicious";
      }
      taste() {
        print "The " + this.flavour + " cake is " + this.adjective + "!";
      }
    }
    
    var cake = Cake();
    cake.flavour = "German chocolate";
    cake.taste(); // Prints "The German chocolate cake is delicious!".
    
    ```

--- jlox/src/main/java/com/craftinginterpreters/lox/LoxFunction.java
+++ jlox/src/main/java/com/craftinginterpreters/lox/LoxFunction.java
@@ -5,8 +5,10 @@ import java.util.List;
 class LoxFunction implements LoxCallable {
     private final Stmt.Function declaration;
     private final Environment closure;
+    private final boolean isInitializer;
 
-    LoxFunction(Stmt.Function declaration, Environment closure) {
+    LoxFunction(Stmt.Function declaration, Environment closure, boolean isInitializer) {
+        this.isInitializer = isInitializer;
         this.declaration = declaration;
         this.closure = closure;
     }
@@ -14,7 +16,7 @@ class LoxFunction implements LoxCallable {
     LoxFunction bind(LoxInstance instance) {
         Environment environment = new Environment(closure);
         environment.define("this", instance);
-        return new LoxFunction(declaration, environment);
+        return new LoxFunction(declaration, environment, isInitializer);
     }
 
     @Override
@@ -34,9 +36,14 @@ class LoxFunction implements LoxCallable {
         try {
             interpreter.executeBlock(declaration.body, environment);
         } catch (Return returnValue) {
+            if (isInitializer) {
+                return closure.getAt(0, "this");
+            }
             return returnValue.value;
         }
-
+        if (isInitializer) {
+            return closure.getAt(0, "this");
+        }
         return null;
     }
 
