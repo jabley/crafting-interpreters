461bcc4cb6c9eb0055b25b95300e04e0fa51d2e3
James Abley <james.abley@gmail.com>
2022-01-02 15:07:04 +0000

    Add rudimentary variable usage
    ```
    > var a = 1; var b = 3; print a + b;
    4
    ```

--- jlox/src/main/java/com/craftinginterpreters/lox/Parser.java
+++ jlox/src/main/java/com/craftinginterpreters/lox/Parser.java
@@ -19,7 +19,7 @@ class Parser {
     List<Stmt> parse() {
         List<Stmt> statements = new ArrayList<>();
         while (!isAtEnd()) {
-            statements.add(statement());
+            statements.add(declaration());
         }
 
         return statements;
@@ -27,6 +27,18 @@ class Parser {
 
     private Expr expression() {
         return equality();
+    }
+
+    private Stmt declaration() {
+        try {
+            if (match(VAR)) {
+                return varDeclaration();
+            }
+            return statement();
+        } catch (ParseError error) {
+            synchronize();
+            return null;
+        }
     }
 
     private Stmt statement() {
@@ -46,6 +58,18 @@ class Parser {
         Expr value = expression();
         consume(SEMICOLON, "Expect ';' after value.");
         return new Stmt.Print(value);
+    }
+
+    private Stmt varDeclaration() {
+        Token name = consume(IDENTIFIER, "Expect variable name.");
+
+        Expr initializer = null;
+        if (match(EQUAL)) {
+            initializer = expression();
+        }
+
+        consume(SEMICOLON, "Expect ';' after variable declaration.");
+        return new Stmt.Var(name, initializer);
     }
 
     private Expr equality() {
@@ -137,6 +161,10 @@ class Parser {
         }
         if (match(NUMBER, STRING)) {
             return new Expr.Literal(previous().literal);
+        }
+
+        if (match(IDENTIFIER)) {
+            return new Expr.Variable(previous());
         }
 
         if (match(LEFT_PAREN)) {
