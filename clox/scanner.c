039f5911a4412bc040f82c50e9b0c9cddad56bcf
James Abley <james.abley@gmail.com>
2022-01-03 23:38:06 +0000

    Add scanning comments

--- clox/scanner.c
+++ clox/scanner.c
@@ -36,6 +36,15 @@ static char peek()
     return *scanner.current;
 }
 
+static char peekNext()
+{
+    if (isAtEnd())
+    {
+        return '\0';
+    }
+    return scanner.current[1];
+}
+
 static bool match(char expected)
 {
     if (isAtEnd())
@@ -86,6 +95,20 @@ static void skipWhitespace()
         case '\n':
             scanner.line++;
             advance();
+            break;
+        case '/':
+            if (peekNext() == '/')
+            {
+                // A comment goes until the end of the line.
+                while (peek() != '\n' && !isAtEnd())
+                {
+                    advance();
+                }
+            }
+            else
+            {
+                return;
+            }
             break;
         default:
             return;
