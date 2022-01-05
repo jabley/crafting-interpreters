a04b48d055140f53783ff96445647de9e7e0fe7a
James Abley <james.abley@gmail.com>
2022-01-05 15:27:32 +0000

    Add support for defining global variables

--- clox/vm.c
+++ clox/vm.c
@@ -34,11 +34,14 @@ void initVM()
 {
     resetStack();
     vm.objects = NULL;
+
+    initTable(&vm.globals);
     initTable(&vm.strings);
 }
 
 void freeVM()
 {
+    freeTable(&vm.globals);
     freeTable(&vm.strings);
     freeObjects();
 }
@@ -84,6 +87,7 @@ static InterpretResult run()
 {
 #define READ_BYTE() (*vm.ip++)
 #define READ_CONSTANT() (vm.chunk->constants.values[READ_BYTE()])
+#define READ_STRING() AS_STRING(READ_CONSTANT())
 #define BINARY_OP(valueType, op)                        \
     do                                                  \
     {                                                   \
@@ -134,6 +138,13 @@ static InterpretResult run()
         case OP_POP:
             pop();
             break;
+        case OP_DEFINE_GLOBAL:
+        {
+            ObjString *name = READ_STRING();
+            tableSet(&vm.globals, name, peek(0));
+            pop();
+            break;
+        }
         case OP_EQUAL:
         {
             Value b = pop();
@@ -203,6 +214,7 @@ static InterpretResult run()
 
 #undef READ_BYTE
 #undef READ_CONSTANT
+#undef READ_STRING
 #undef BINARY_OP
 }
 
