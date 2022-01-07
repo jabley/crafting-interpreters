fcf0137be7f5957e790f2a5c52489899c403b8e0
James Abley <james.abley@gmail.com>
2022-01-07 17:07:46 +0000

    Handle compiling multiple nested functions
    Add a linked list of Compiler references. When we start compiling a
    function declaration, we create a new Compiler, initialize it and set it
    to be the current Compiler. As we compile the function body, bytecode is
    emitted into the chunk owned by the new Compiler's function. At the end
    of the function block, we call `endCompiler` which returns the new
    compiled function object to be stored in the surrounding function's
    constant table.

--- clox/compiler.c
+++ clox/compiler.c
@@ -54,8 +54,9 @@ typedef enum
     TYPE_SCRIPT
 } FunctionType;
 
-typedef struct
+typedef struct Compiler
 {
+    struct Compiler *enclosing;
     ObjFunction *function;
     FunctionType type;
 
@@ -222,6 +223,7 @@ static void patchJump(int offset)
 
 static void initCompiler(Compiler *compiler, FunctionType type)
 {
+    compiler->enclosing = current;
     compiler->function = NULL;
     compiler->type = type;
     compiler->localCount = 0;
@@ -249,6 +251,7 @@ static ObjFunction *endCompiler()
     }
 #endif
 
+    current = current->enclosing;
     return function;
 }
 
