4e11d8750bfe49f0de0740522f5b6c1c01f7f5b7
James Abley <james.abley@gmail.com>
2022-01-09 12:29:06 +0000

    Add a subset of working closures
    As long as all of the variables remain on the stack, we have working
    closures with this change. Closing that gap will be done in the next
    commit!
    
    We are using the same solution described for Lua. We are keeping the
    constraint of having a single-pass compiler, so we want something that
    works with that.
    
    This change introduces a level of indirection called an 'upvalue'. An
    upvalue refers to a local variable in an enclosing function. Every
    closure maintains a dynamic array of upvalues; one for each surrounding
    local variable that the closure uses.
    
    The upvalue points back into the stack to where the variable it captured
    lives. When the closure needs to access a closed-over variable, it goes
    through the corresponding upvalue to reach it. When a function
    declaration is first executed and we create a closure for it, the VM
    creates the array of upvalues and wires them up to "capture" the
    surrounding local variables that the closure needs.
    
    Upvalues serve as the layer of indirection needed to continue to find a
    captured local variable even after it moves off the stack.
    
    We want to do as much work as possible during compilation, so that
    execution is simple and fast.
    
    We had the object model and opcode changes needed. The complexity is
    added to the compiler, which has to recursively resolve upvalues to
    ensure values that are multiple scopes outside of a function are
    correctly resolved.
    
    The OP_CLOSURE instruction is unique in that it has a variably sized
    encoding. For each upvalue the closure captures, there are two
    single-byte operands. Each pair of operands specifies what that upvalue
    captures. If the first byte is one, it captures a local variable in the
    enclosing function. If zero, it captures one of the function’s upvalues.
    The next byte is the local slot or upvalue index to capture.

--- clox/object.c
+++ clox/object.c
@@ -21,8 +21,17 @@ static Obj *allocateObject(size_t size, ObjType type)
 
 ObjClosure *newClosure(ObjFunction *function)
 {
+    ObjUpvalue **upvalues = ALLOCATE(ObjUpvalue *,
+                                     function->upvalueCount);
+    for (int i = 0; i < function->upvalueCount; i++)
+    {
+        upvalues[i] = NULL;
+    }
+
     ObjClosure *closure = ALLOCATE_OBJ(ObjClosure, OBJ_CLOSURE);
     closure->function = function;
+    closure->upvalues = upvalues;
+    closure->upvalueCount = function->upvalueCount;
     return closure;
 }
 
@@ -30,6 +39,7 @@ ObjFunction *newFunction()
 {
     ObjFunction *function = ALLOCATE_OBJ(ObjFunction, OBJ_FUNCTION);
     function->arity = 0;
+    function->upvalueCount = 0;
     function->name = NULL;
     initChunk(&function->chunk);
     return function;
@@ -95,6 +105,13 @@ ObjString *copyString(const char *chars, int length)
     return allocateString(heapChars, length, hash);
 }
 
+ObjUpvalue *newUpvalue(Value *slot)
+{
+    ObjUpvalue *upvalue = ALLOCATE_OBJ(ObjUpvalue, OBJ_UPVALUE);
+    upvalue->location = slot;
+    return upvalue;
+}
+
 static void printFunction(ObjFunction *function)
 {
     if (function->name == NULL)
@@ -121,6 +138,9 @@ void printObject(Value value)
         break;
     case OBJ_STRING:
         printf("%s", AS_CSTRING(value));
+        break;
+    case OBJ_UPVALUE:
+        printf("upvalue");
         break;
     }
 }
