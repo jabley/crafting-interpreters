5951c3209e1081235dfa73256518235beef5ee6d
James Abley <james.abley@gmail.com>
2022-01-11 17:09:38 +0000

    Trace references
    Once we have marked all the roots, we can do this next phase. This is to
    trace the references.
    
    This is straightforward translation of the textual algorithm. We pop
    gray objects off the stack until it's empty. For each gray object, we
    traverse the references and then mark it black. Traversing an object's
    references may turn up new white objects that get marked gray and added
    to the stack. So this function swings back and forth between turning
    white objects gray and gray objects black, gradually advancing the
    entire wavefront forward.
    
    We also need to detect cycles. References between objects are directed,
    but that doesn't mean they're acyclic! It's entirely possible to have
    cycles of objects. When that happens, we need to ensure our collector
    doesn't get stuck in an infinite loop as it continually re-adds the same
    series of objects to the gray stack.
    
    If the object is already marked, we don't mark it again and thus don't
    add it to the gray stack. This ensures that an already-gray object is
    not redundantly added and that a black object is not inadvertently
    turned back to gray. In other words, it keeps the wavefront moving
    forward through only the white objects.

--- clox/memory.c
+++ clox/memory.c
@@ -40,6 +40,12 @@ void markObject(Obj *object)
         return;
     }
 
+    // detect cycles and ensure we don't get stuck in an infinitely loop
+    if (object->isMarked)
+    {
+        return;
+    }
+
 #ifdef DEBUG_LOG_GC
     printf("%p mark ", (void *)object);
     printValue(OBJ_VAL(object));
@@ -70,6 +76,50 @@ void markValue(Value value)
     if (IS_OBJ(value))
     {
         markObject(AS_OBJ(value));
+    }
+}
+
+static void markArray(ValueArray *array)
+{
+    for (int i = 0; i < array->count; i++)
+    {
+        markValue(array->values[i]);
+    }
+}
+
+static void blackenObject(Obj *object)
+{
+#ifdef DEBUG_LOG_GC
+    printf("%p blacken ", (void *)object);
+    printValue(OBJ_VAL(object));
+    printf("\n");
+#endif
+
+    switch (object->type)
+    {
+    case OBJ_CLOSURE:
+    {
+        ObjClosure *closure = (ObjClosure *)object;
+        markObject((Obj *)closure->function);
+        for (int i = 0; i < closure->upvalueCount; i++)
+        {
+            markObject((Obj *)closure->upvalues[i]);
+        }
+        break;
+    }
+    case OBJ_FUNCTION:
+    {
+        ObjFunction *function = (ObjFunction *)object;
+        markObject((Obj *)function->name);
+        markArray(&function->chunk.constants);
+        break;
+    }
+    case OBJ_UPVALUE:
+        markValue(((ObjUpvalue *)object)->closed);
+        break;
+    case OBJ_NATIVE:
+    case OBJ_STRING:
+        break;
     }
 }
 
@@ -135,6 +185,15 @@ static void markRoots()
     markCompilerRoots();
 }
 
+static void traceReferences()
+{
+    while (vm.grayCount > 0)
+    {
+        Obj *object = vm.grayStack[--vm.grayCount];
+        blackenObject(object);
+    }
+}
+
 void collectGarbage()
 {
 #ifdef DEBUG_LOG_GC
@@ -142,6 +201,7 @@ void collectGarbage()
 #endif
 
     markRoots();
+    traceReferences();
 
 #ifdef DEBUG_LOG_GC
     printf("-- gc end\n");
