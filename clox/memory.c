031f9dcfcabc2d05bd5ed3eb26ea80c5d05981b6
James Abley <james.abley@gmail.com>
2022-01-12 16:45:04 +0000

    Add object model for method declarations

--- clox/memory.c
+++ clox/memory.c
@@ -110,6 +110,7 @@ static void blackenObject(Obj *object)
     {
         ObjClass *klass = (ObjClass *)object;
         markObject((Obj *)klass->name);
+        markTable(&klass->methods);
         break;
     }
     case OBJ_CLOSURE:
@@ -155,6 +156,8 @@ static void freeObject(Obj *object)
     {
     case OBJ_CLASS:
     {
+        ObjClass *klass = (ObjClass *)object;
+        freeTable(&klass->methods);
         FREE(ObjClass, object);
         break;
     }
