1de302f01a88eafb7a0e957137674d4d793931ed
James Abley <james.abley@gmail.com>
2022-01-17 23:01:37 +0000

    Complete allocator
    I want to break this up into a series of commits. For now, just copying
    the book implementation until I have time to understand it a bit more.

--- rlox/stickyimmix/src/blockmeta.rs
+++ rlox/stickyimmix/src/blockmeta.rs
@@ -9,6 +9,38 @@ pub struct BlockMeta {
 // ANCHOR_END: DefBlockMeta
 
 impl BlockMeta {
+    /// Heap allocate a metadata instance so that it doesn't move so we can store pointers
+    /// to it.
+    pub fn new_boxed() -> Box<BlockMeta> {
+        Box::new(BlockMeta {
+            line_mark: [false; constants::LINE_COUNT],
+            block_mark: false,
+        })
+    }
+
+    /// Mark the indexed line
+    pub fn mark_line(&mut self, index: usize) {
+        self.line_mark[index] = true;
+    }
+
+    /// Indicate the entire block as marked
+    pub fn mark_block(&mut self) {
+        self.block_mark = true;
+    }
+
+    /// Reset all mark flags to unmarked.
+    pub fn reset(&mut self) {
+        for bit in self.line_mark.iter_mut() {
+            *bit = false
+        }
+        self.block_mark = false;
+    }
+
+    /// Return an iterator over all the line mark flags
+    pub fn line_iter(&self) -> impl Iterator<Item = &'_ bool> {
+        self.line_mark.iter()
+    }
+
     /// Given a byte index into a block (the `starting_at` parameter) find the next available
     /// hole in which bump allocation can occur, or `None` if no hole can be found in this
     /// block.
@@ -65,4 +97,113 @@ impl BlockMeta {
         None
     }
     // ANCHOR_END: DefFindNextHole
+}
+
+#[cfg(test)]
+mod tests {
+
+    use super::*;
+
+    #[test]
+    fn test_find_next_hole() {
+        // A set of marked lines with a couple holes.
+        // The first hole should be seen as conservatively marked.
+        // The second hole should be the one selected.
+        let mut meta = BlockMeta::new_boxed();
+
+        meta.mark_line(0);
+        meta.mark_line(1);
+        meta.mark_line(2);
+        meta.mark_line(4);
+        meta.mark_line(10);
+
+        let expect = Some((6 * constants::LINE_SIZE, 10 * constants::LINE_SIZE));
+
+        let got = meta.find_next_available_hole(0);
+
+        println!("test_find_next_hole got {:?} expected {:?}", got, expect);
+
+        assert!(got == expect);
+    }
+
+    #[test]
+    fn test_find_next_hole_at_line_zero() {
+        // Should find the hole starting at the beginning of the block
+        let mut meta = BlockMeta::new_boxed();
+
+        meta.mark_line(3);
+        meta.mark_line(4);
+        meta.mark_line(5);
+
+        let expect = Some((0, 3 * constants::LINE_SIZE));
+
+        let got = meta.find_next_available_hole(0);
+
+        println!(
+            "test_find_next_hole_at_line_zero got {:?} expected {:?}",
+            got, expect
+        );
+
+        assert!(got == expect);
+    }
+
+    #[test]
+    fn test_find_next_hole_at_block_end() {
+        // The first half of the block is marked.
+        // The second half of the block should be identified as a hole.
+        let mut meta = BlockMeta::new_boxed();
+
+        let halfway = constants::LINE_COUNT / 2;
+
+        for i in 0..halfway {
+            meta.mark_line(i);
+        }
+
+        let expect = Some(((halfway + 1) * constants::LINE_SIZE, constants::BLOCK_SIZE));
+
+        let got = meta.find_next_available_hole(0);
+
+        println!(
+            "test_find_next_hole_at_block_end got {:?} expected {:?}",
+            got, expect
+        );
+
+        assert!(got == expect);
+    }
+
+    #[test]
+    fn test_find_hole_all_conservatively_marked() {
+        // Every other line is marked.
+        // No hole should be found.
+        let mut meta = BlockMeta::new_boxed();
+
+        for i in 0..constants::LINE_COUNT {
+            if i % 2 == 0 {
+                // there is no stable step function for range
+                meta.mark_line(i);
+            }
+        }
+
+        let got = meta.find_next_available_hole(0);
+
+        println!(
+            "test_find_hole_all_conservatively_marked got {:?} expected None",
+            got
+        );
+
+        assert!(got == None);
+    }
+
+    #[test]
+    fn test_find_entire_block() {
+        // No marked lines. Entire block is available.
+        let meta = BlockMeta::new_boxed();
+
+        let expect = Some((0, constants::BLOCK_SIZE));
+        let got = meta.find_next_available_hole(0);
+
+        println!("test_find_entire_block got {:?} expected {:?}", got, expect);
+
+        assert!(got == expect);
+    }
 }
